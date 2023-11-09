1ce3d3de063f63ed8554da9933ac0ab636609e6d
emiru73 <moon1113uranus@ezweb.ne.jp>
2023-11-09 14:35:37 +0900

    コード追加

---
+++ code2_1.py
@@ -0,0 +1,8 @@
+network = 88
+database = 95
+security = 90
+total = network + database + security
+avg = total / 3
+# avg = total / 3  マジックナンバー
+print('合計点:{}'.format(total))
+print('平均点:{}'.format(avg))
